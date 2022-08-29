edb382c6b636280ed8a51bd38b2822572c7cad32
AgusNarvaez02 <103900236+AgusNarvaez02@users.noreply.github.com>
2022-08-28 22:58:37 -0300

    Update Ejercicio5.py

--- Unidad2/Pila/Ejercicio5/Ejercicio5.py
+++ Unidad2/Pila/Ejercicio5/Ejercicio5.py
@@ -1,15 +1,26 @@
-class Pila:
+
+
+class Torre:
     __lista=None
 
     def __init__(self):
         self.__lista= []
 
-    def insertar(self, dato):
-        self.__lista.append(dato)
 
+    def vacio(self):
+        return len(self.__lista)==0
+
+    def añadirDisco(self, elemento):
+        if self.vacio() or elemento<= self.__lista[-1]:
+            print('ingresa al if')
+            self.__lista.append(elemento)
+        else:
+            print('No puede colocar un disco mas grande encima de uno mas pequeño ')
+            raise Exception('No puede colocar un disco mas grande encima de uno mas pequeño ')
 
     def tamaño(self):
         return len(self.__lista)
+
 
     def eliminarvalor(self):
         if len(self.__lista)==0:
@@ -18,6 +29,10 @@ class Pila:
         valor = self.__lista.pop()
         return valor
 
+    def TamañoDisco(self, i):
+        if len(self.__lista)<i:
+            return ''
 
-class TorredeHanoi:
-    __torre= list[Pila]
+        return str(self.__lista[i-1])
+
+
