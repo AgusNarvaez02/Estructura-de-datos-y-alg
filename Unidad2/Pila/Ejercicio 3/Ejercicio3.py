fc7b08058a58f557a6c045ebc2a8e1801133c8c9
AgusNarvaez02 <103900236+AgusNarvaez02@users.noreply.github.com>
2022-08-30 12:56:17 -0300

    Update Ejercicio3.py

--- Unidad2/Pila/Ejercicio 3/Ejercicio3.py
+++ Unidad2/Pila/Ejercicio 3/Ejercicio3.py
@@ -1,24 +1,32 @@
-class Pila:
-    __lista=None
+import numpy as np
 
-    def __init__(self):
-        self.__lista= []
+class Pila:
+    __arreglo=None
+    __tope=0
+    __tamaño: int
+
+    def __init__(self, tamaño: int=20):
+        self.__arreglo= np.empty(tamaño, dtype=int)
+        self.__tope= 0
+        self.__tamaño=tamaño
+
 
     def insertar(self, dato):
-        self.__lista.append(dato)
+        if self.__tope== self.__tamaño:
+            raise Exception('Pila llena')
 
+        self.__arreglo[self.__tope]= dato
+        self.__tope+=1
 
-    def tamaño(self):
-        return len(self.__lista)
+    def getTam(self):
+        return self.__tope
 
     def eliminarvalor(self):
-        if len(self.__lista)==0:
-            raise Exception('Lista vacia')
+        if self.__tope==0:
+            raise Exception('Pila vacia')
 
-        valor = self.__lista.pop()
-        return valor
-
-
+        self.__tope-=1
+        return self.__arreglo[self.__tope]
 def factorial(num):
     pila=Pila()
     val=num
@@ -26,9 +34,8 @@ def factorial(num):
     while num>1:
         resta=num-1
         pila.insertar(resta)
-        print(resta)
         num=resta
-    while pila.tamaño()!=0:
+    while pila.getTam()!=0:
         val*=pila.eliminarvalor()
     print(val)
 
