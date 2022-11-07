e59c9c52ea9bdc7bc36f2d4b19d85093f1a9f171
AgusNarvaez02 <103900236+AgusNarvaez02@users.noreply.github.com>
2022-11-07 12:38:46 -0300

    Create Nodo.py

---
+++ UnidadN6/Ejercicio1/GrafoEnlazado/Nodo.py
@@ -0,0 +1,47 @@
+class Nodo:
+    __dato= None
+    __siguiente=None
+
+
+    def __init__(self, dato):
+        self.__dato=dato
+        self.__siguiente=None
+
+    def getDato(self):
+        return self.__dato
+
+    def getSiguiente(self):
+        return self.__siguiente
+
+    def setSig(self, sig):
+        self.__siguiente=sig
+
+    def setDato(self, date):
+        self.__dato=date
+
+
+class Lista:
+    __cabeza=Nodo
+
+    def __init__(self):
+        self.__cabeza=None
+
+    def Insertar(self, date):
+        Nuevo= Nodo(date)
+        if self.__cabeza==None:
+            self.__cabeza= Nuevo
+
+        elif not self.Relacionado(date):
+            Nuevo.setSig(self.__cabeza)
+            self.__cabeza=Nuevo
+
+    def Relacionado(self, date):
+        aux= self.__cabeza
+        rep=False
+        while aux!=None and not rep:
+            if aux.getDato()==date:
+                rep=True
+
+            aux= aux.getSiguiente()
+
+        return rep
